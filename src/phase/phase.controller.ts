d5026c6091fec98c178b203559ee709ea970d111
MaksBykov <maks.bmv.2011@mail.ru>
2023-07-13 15:19:21 +0700

    g

--- src/phase/phase.controller.ts
+++ src/phase/phase.controller.ts
@@ -1,37 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { PhaseService } from './phase.service';
 import { CreatePhaseDto } from './dto/create-phase.dto';
 import { UpdatePhaseDto } from './dto/update-phase.dto';
+import JwtAuthenticationGuard from 'src/auth/jwt-auth.guard';
 
 @Controller('phase')
 export class PhaseController {
   constructor(private readonly phaseService: PhaseService) {}
 
+  @UseGuards(JwtAuthenticationGuard)
   @Post()
   create(@Body() createPhaseDto: CreatePhaseDto) {
     return this.phaseService.create(createPhaseDto);
   }
 
+  @UseGuards(JwtAuthenticationGuard)
   @Get()
   findAll() {
     return this.phaseService.findAll();
   }
 
+  @UseGuards(JwtAuthenticationGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.phaseService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthenticationGuard)
   @Get('project/:id')
   findprojectid(@Param('id') id: string) {
     return this.phaseService.findprojectid(+id);
   }
 
+  @UseGuards(JwtAuthenticationGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePhaseDto: UpdatePhaseDto) {
     return this.phaseService.update(+id, updatePhaseDto);
   }
 
+  @UseGuards(JwtAuthenticationGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.phaseService.remove(+id);
